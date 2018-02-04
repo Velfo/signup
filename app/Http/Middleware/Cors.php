ded6b833d248fc5f67064d2791a1ab612fe50949
vagrant <vagrant@laravelsignup>
2018-02-04 04:23:15 +0000

    [php][cors][middleware] added improved cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,18 +13,17 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        $domains = ['http://localhost:8081'];
+     public function handle($request, Closure $next)
+     {
+         $domains = ['http://localhost:8081'];
 
-        if (isset($request->server()['HTTP_ORIGIN'])) {
-          $origin = $request->server()['HTTP_ORIGIN'];
-          if (in_array($origin, $domains)) {
-            header('Access-Control-Allow-Origin: ' . $origin);
-            header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
-          }
-
-        }
-        return $next($request);
-    }
+         if (isset($request->server()['HTTP_ORIGIN'])) {
+           $origin = $request->server()['HTTP_ORIGIN'];
+           if(in_array($origin, $domains)) {
+             header('Access-Control-Allow-Origin: ' . $origin);
+             header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+           }
+         }
+         return $next($request);
+     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::get('colour-test', function () {
     return response(["blue","red","green","silver"], 200);
 });
 
+Route::get('test', function () {
+  return response([1,2,3,4], 200);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
